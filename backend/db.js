5d4850a4462d3d33614c030403871710d555fd80
ashmikan <ashmika.nathali123@gmail.com>
2026-02-19 22:51:46 +0530

    add database connection setup

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "expense_tracker"
+
+});
+
+db.connect((err) => {
+
+  if(err)
+    console.log(err);
+
+  else
+    console.log("MySQL Connected");
+
+});
+
+module.exports = db;
