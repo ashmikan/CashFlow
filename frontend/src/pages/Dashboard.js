e05b0d34b71963abda2ad95912519d2c77717c70
ashmikan <ashmika.nathali123@gmail.com>
2026-02-23 21:43:39 +0530

    refactor Dashboard for adding transactions

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -3,23 +3,46 @@ import {useEffect,useState} from "react";
 
 function Dashboard(){
 
+const loadTransactions = async()=>{
+
+const res = await axios.get(
+"http://localhost:5000/api/transactions",
+{
+headers:{Authorization:token}
+}
+);
+
+setTransactions(res.data);
+
+};
+
+const addTransaction = async()=>{
+
+await axios.post(
+"http://localhost:5000/api/transactions",
+{text,amount,category},
+{
+headers:{Authorization:token}
+}
+);
+
+loadTransactions();
+
+};    
+
 const [transactions,setTransactions]
 = useState([]);
+
+const [text,setText] = useState("");
+const [amount,setAmount] = useState("");
+const [category,setCategory] = useState("");
 
 const token =
 localStorage.getItem("token");
 
 useEffect(()=>{
 
-axios.get(
-"http://localhost:5000/api/transactions",
-{
-headers:{Authorization:token}
-}
-)
-.then(res=>{
-setTransactions(res.data);
-});
+loadTransactions();
 
 },[]);
 
@@ -29,13 +52,30 @@ return(
 
 <h2>Dashboard</h2>
 
-{transactions.map(t=>(
+<h4>Add Transaction</h4>
 
-<div key={t.id}>
-{t.text} - Rs.{t.amount}
-</div>
+<input
+placeholder="Description"
+onChange={e=>setText(e.target.value)}
+/>
 
-))}
+<input
+placeholder="Amount (Rs.)"
+type="number"
+onChange={e=>setAmount(e.target.value)}
+/>
+
+<select
+onChange={e=>setCategory(e.target.value)}
+>
+
+<option>Food</option>
+<option>Transport</option>
+<option>Salary</option>
+<option>Shopping</option>
+<option>Bills</option>
+
+</select>
 
 </div>
 
